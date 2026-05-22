cb008a25a06941efddb2ffd54ca7d9b2b06cbd7c
alma1070 <almaslowcore@gmail.com>
2026-05-22 21:50:09 +0700

    Navigation + Theme

--- app/src/main/java/com/almaslowcore/oasis/ui/theme/Type.kt
+++ app/src/main/java/com/almaslowcore/oasis/ui/theme/Type.kt
@@ -1,14 +1,10 @@
 package com.almaslowcore.oasis.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-import com.almaslowcore.oasis.R
-
-import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import com.almaslowcore.oasis.R
 
 val provider = GoogleFont.Provider(
     providerAuthority = "com.google.android.gms.fonts",
